5402ee32cd579c122a9386d51dd3a8cb143c39b3
tega <alessiotegoni4@gmail.com>
2025-07-03 19:37:35 +0200

    fix: reset credits server action

--- src/features/(league)/(admin)/handle-credits/actions/handle-credits.ts
+++ src/features/(league)/(admin)/handle-credits/actions/handle-credits.ts
@@ -6,6 +6,13 @@ import {
   ResetCreditsSchema,
 } from "../schema/handle-credits";
 import { canUpdateCredits } from "../permissions/handle-credits";
+import { getLeagueTeams } from "@/features/(league)/teams/queries/leagueTeam";
+import { updateLeagueTeam } from "@/features/(league)/teams/db/leagueTeam";
+import { createSuccess } from "@/lib/helpers";
+
+enum HANDLE_CREDITS_MESSAGES {
+  RESET_SUCCESS = "Crediti resettati con successo",
+}
 
 export async function resetCredits(values: ResetCreditsSchema) {
   const { isValid, error, data } = validateSchema<ResetCreditsSchema>(
@@ -16,4 +23,13 @@ export async function resetCredits(values: ResetCreditsSchema) {
 
   const permissions = await canUpdateCredits(data.leagueId);
   if (permissions.error) return permissions.error;
+
+  const leagueTeams = await getLeagueTeams(data.leagueId);
+  const leagueTeamsIds = leagueTeams.map((team) => team.id);
+
+  await updateLeagueTeam(leagueTeamsIds, data.leagueId, {
+    credits: data.credits,
+  });
+
+  return createSuccess(HANDLE_CREDITS_MESSAGES.RESET_SUCCESS, null);
 }
