8c224bec1b3c5ad60c8a212132453099e66477e9
tega <alessiotegoni4@gmail.com>
2025-07-03 20:19:33 +0200

    fix: ResetCreditsDialog

--- src/features/(league)/(admin)/handle-credits/components/ResetCreditsDialog.tsx
+++ src/features/(league)/(admin)/handle-credits/components/ResetCreditsDialog.tsx
@@ -12,8 +12,17 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { use, useEffect, useState } from "react";
+import { use } from "react";
 import { resetCredits } from "../actions/handle-credits";
+import { useForm } from "react-hook-form";
+import {
+  resetCreditsSchema,
+  ResetCreditsSchema,
+} from "../schema/handle-credits";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import SubmitButton from "@/components/SubmitButton";
+import useActionToast from "@/hooks/useActionToast";
 
 export default function ResetCreditsDialog({
   leagueId,
@@ -22,17 +31,27 @@ export default function ResetCreditsDialog({
   leagueId: string;
   defaultCreditsPromise: Promise<number>;
 }) {
-  const [credits, setCredits] = useState<number>(use(defaultCreditsPromise));
+  const toast = useActionToast();
 
-  //   const defaultCredits = use(defaultCreditsPromise);
+  const form = useForm<ResetCreditsSchema>({
+    resolver: zodResolver(resetCreditsSchema),
+    defaultValues: {
+      leagueId,
+      credits: use(defaultCreditsPromise) ?? 500,
+    },
+  });
 
-  //   useEffect(() => {
-  //     setCredits(defaultCredits);
-  //   }, [defaultCredits]);
+  console.log(form.getValues());
+  
+
+  async function onSubmit(data: ResetCreditsSchema) {
+    const res = await resetCredits(data);
+    toast(res);
+  }
 
   return (
-    <Dialog>
-      <Button asChild>
+    <Dialog open={form.formState.isSubmitting ? true : undefined}>
+      <Button asChild className="w-fit">
         <DialogTrigger>Resetta crediti</DialogTrigger>
       </Button>
       <DialogContent>
@@ -43,20 +62,17 @@ export default function ResetCreditsDialog({
             te nelle impostazioni generali della lega
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <DialogFooter>
+              <DialogClose asChild className="sm:w-fit">
+                <Button variant="outline">Chiudi</Button>
+              </DialogClose>
+              <SubmitButton className="sm:w-fit">Resetta</SubmitButton>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
-      <DialogFooter>
-        <DialogClose asChild>
-          <Button variant="outline">Chiudi</Button>
-        </DialogClose>
-        <ActionButton
-          action={resetCredits.bind(null, {
-            leagueId,
-            credits,
-          })}
-        >
-          Resetta
-        </ActionButton>
-      </DialogFooter>
     </Dialog>
   );
 }
