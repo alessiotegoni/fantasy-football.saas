c31ec46eb9e9211e093c68c3a68e226deafa5352
tega <alessiotegoni4@gmail.com>
2025-07-07 11:11:17 +0200

    add: canDeleteCalendar permission function

--- src/features/(league)/(admin)/calendar/permissions/calendar.ts
+++ src/features/(league)/(admin)/calendar/permissions/calendar.ts
@@ -1,17 +1,33 @@
+import { getLeagueAdmin } from "@/features/(league)/leagues/queries/league";
 import { getLeagueTeams } from "@/features/(league)/teams/queries/leagueTeam";
 import { getUpcomingSplit } from "@/features/splits/queries/split";
 import { createError, createSuccess } from "@/lib/helpers";
 
 enum GENERATE_CALENDAR_MESSAGES {
-  SPLIT_NOT_UNCOMING = "Puoi generare il calendario solo quando lo split non e' ancora iniziato",
+  REQUIRE_ADMIN = "Per gestire il calendario devi essere un admin della lega",
+  SPLIT_NOT_UNCOMING = "Puoi gestire il calendario solo quando lo split non e' ancora iniziato",
   INVALID_TEAMS_LENGTH = "Per generare il calendario la lega deve avere almeno 4 squadre",
 }
 
-export async function canGenerateCalendar(leagueId: string) {
-  const [upcomingSplit, leagueTeams] = await Promise.all([
+export async function validateBaseRequirements(
+  userId: string,
+  leagueId: string
+) {
+  const isAdmin = await getLeagueAdmin(userId, leagueId);
+
+  if (!isAdmin) return createError(GENERATE_CALENDAR_MESSAGES.REQUIRE_ADMIN);
+
+  return createSuccess("", null);
+}
+
+export async function canGenerateCalendar(userId: string, leagueId: string) {
+  const [baseValidation, upcomingSplit, leagueTeams] = await Promise.all([
+    validateBaseRequirements(userId, leagueId),
     getUpcomingSplit(),
     getLeagueTeams(leagueId),
   ]);
+
+  if (baseValidation.error) return baseValidation;
 
   if (!upcomingSplit) {
     return createError(GENERATE_CALENDAR_MESSAGES.SPLIT_NOT_UNCOMING);
@@ -19,6 +35,21 @@ export async function canGenerateCalendar(leagueId: string) {
 
   if (leagueTeams.length < 4) {
     return createError(GENERATE_CALENDAR_MESSAGES.INVALID_TEAMS_LENGTH);
+  }
+
+  return createSuccess("", null);
+}
+
+export async function canDeleteCalendar(userId: string, leagueId: string) {
+  const [baseValidation, upcomingSplit] = await Promise.all([
+    validateBaseRequirements(userId, leagueId),
+    getUpcomingSplit(),
+  ]);
+
+  if (baseValidation.error) return baseValidation;
+
+  if (!upcomingSplit) {
+    return createError(GENERATE_CALENDAR_MESSAGES.SPLIT_NOT_UNCOMING);
   }
 
   return createSuccess("", null);
