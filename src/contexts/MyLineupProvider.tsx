d9b86d3d02f48c3be2a7d0a0368d858e1fc4588f
tega <alessiotegoni4@gmail.com>
2025-07-14 11:47:28 +0200

    fix: MatchLineupProvider

---
+++ src/contexts/MyLineupProvider.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { createContext, useCallback, useState } from "react";
+
+type LineupPlayer = {
+  id: string;
+  playerId: string;
+  roleId: number;
+  positionId: string;
+  positionOrder: number;
+  totalVote: number;
+};
+
+type MyLineup = {
+  id: string;
+  benchPlayers: LineupPlayer[];
+  starterPlayers: LineupPlayer[];
+} | null;
+
+type MyLineupContext = {
+  myLineup: MyLineup;
+  handleSetLineup: (lineup: MyLineup) => void;
+};
+
+const MyLineupContext = createContext<MyLineupContext | null>(null);
+
+export default function MyLineupProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [myLineup, setMyLineup] = useState<MyLineup>(null);
+
+  const handleSetLineup = useCallback(
+    (lineup: MyLineup) => setMyLineup(lineup),
+    []
+  );
+
+  return (
+    <MyLineupContext.Provider value={{ myLineup, handleSetLineup }}>
+      {children}
+    </MyLineupContext.Provider>
+  );
+}
