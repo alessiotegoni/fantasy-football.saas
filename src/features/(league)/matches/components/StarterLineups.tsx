02d663dd5b2593b4a2c835481901d4dab5b9a565
tega <alessiotegoni4@gmail.com>
2025-07-16 16:51:12 +0200

    add: lineupEmptyState component

--- src/features/(league)/matches/components/StarterLineups.tsx
+++ src/features/(league)/matches/components/StarterLineups.tsx
@@ -3,6 +3,7 @@ import { WarningTriangle } from "iconoir-react";
 import { SplitMatchday } from "@/features/splits/queries/split";
 import { LineupTeam } from "../utils/match";
 import StarterLineupField from "./StarterLineupField";
+import LineupEmptyState from "./LineupEmptyState";
 
 type Props = {
   match: MatchInfo;
@@ -65,31 +66,6 @@ export default async function StarterLineups({
           );
         }
       })}
-    </div>
-  );
-}
-
-function LineupEmptyState({
-  myTeam,
-  team,
-  matchMatchday,
-}: {
-  team: NonNullable<LineupTeam>;
-  myTeam: LineupTeam | undefined;
-  matchMatchday: SplitMatchday;
-}) {
-  const isMatchdayEnded = matchMatchday.status === "ended";
-  const text =
-    myTeam?.id !== team.id
-      ? "Formazione non inserita"
-      : `Non puoi ${isMatchdayEnded ? "piu'" : "ancora"} mettere la formazione`;
-
-  return (
-    <div className="flex justify-center items-center">
-      <div className="max-w-40 bg-muted rounded-3xl text-sm text-center flex flex-col justify-center items-center p-4 border border-border">
-        <WarningTriangle className="size-12 mb-3" />
-        {text}
-      </div>
     </div>
   );
 }
