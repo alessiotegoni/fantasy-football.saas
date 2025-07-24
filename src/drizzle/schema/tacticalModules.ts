95ad3070a00f62b5e8429c20647f8f97b256f391
tega <alessiotegoni4@gmail.com>
2025-07-24 16:49:03 +0200

    fix: tacticalModule db schema

--- src/drizzle/schema/tacticalModules.ts
+++ src/drizzle/schema/tacticalModules.ts
@@ -1,12 +1,14 @@
 import { pgTable, text, smallint, jsonb } from "drizzle-orm/pg-core";
+import { PRESIDENT_ROLE_ID } from "./playerRoles";
 
 export const positions = ["PR", "GK", "FB", "MF", "ST"] as const;
+
+export const PRESIDENT_POSITION_ID = `${positions[0]}-${PRESIDENT_ROLE_ID}`;
 
 export type Position = (typeof positions)[number];
 export type PositionId = `${Position}-${number}`;
 
 export type RolePosition = {
-  count: number;
   roleId: number;
   positionsIds: PositionId[];
 };
