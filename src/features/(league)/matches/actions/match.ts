d8b49b5b1b5c347a2ebd899c3a6126d22a1da5af
tega <alessiotegoni4@gmail.com>
2025-07-25 14:16:27 +0200

    add: canSaveLineup permission fn

--- src/features/(league)/matches/actions/match.ts
+++ src/features/(league)/matches/actions/match.ts
@@ -1,10 +1,23 @@
 "use server";
 
-import { createSuccess } from "@/lib/helpers";
+import { createError, createSuccess } from "@/lib/helpers";
 import { matchLineupSchema, MatchLineupSchema } from "../schema/match";
+import { VALIDATION_ERROR } from "@/schema/helpers";
+import { getUserId } from "@/features/users/utils/user";
+import { canSaveLineup } from "../permissions/match";
+import { db } from "@/drizzle/db";
+import { deleteLineupPlayers, insertLineup, insertLineupPlayers } from "../db/match";
 
 export async function saveLineup(values: MatchLineupSchema) {
-  const { success, data } = await matchLineupSchema.safeParseAsync(values)
-    console.log(data);
-    return createSuccess("", null)
+  const { success, data } = await matchLineupSchema.safeParseAsync(values);
+  if (!success) return createError(VALIDATION_ERROR);
+
+  const userId = await getUserId();
+  if (!userId) return createError(VALIDATION_ERROR);
+
+  const validation = await canSaveLineup({ ...data, userId });
+  if (validation.error) return validation;
+
+  console.log(data);
+  return createSuccess("", null);
 }
---
+++ src/features/(league)/matches/permissions/match.ts
@@ -0,0 +1,45 @@
+import { getUserTeamId } from "@/features/users/queries/user";
+import { MatchLineupSchema } from "../schema/match";
+import { getMatchInfo } from "../queries/match";
+import { getTeamsPlayers } from "../../teamsPlayers/queries/teamsPlayer";
+import { createError, createSuccess } from "@/lib/helpers";
+
+enum LINEUP_ERRORS {
+  MATCH_NOT_FOUND = "Partita non trovata",
+  USER_TEAM_NOT_FOUND = "La tua squadra non e' stata trovata",
+  INVALID_TEAM = "Non partecipa a questa partita",
+  INVALID_PLAYERS = "Alcuni giocatori scelti nella formazione non fanno parte della tua squadra",
+}
+
+export async function canSaveLineup({
+  leagueId,
+  userId,
+  matchId,
+  lineupPlayers,
+}: MatchLineupSchema & { userId: string }) {
+  const [matchInfo, userTeamId] = await Promise.all([
+    getMatchInfo({ leagueId, matchId }),
+    getUserTeamId(userId, leagueId),
+  ]);
+
+  if (!matchInfo) return createError(LINEUP_ERRORS.MATCH_NOT_FOUND);
+  if (!userTeamId) return createError(LINEUP_ERRORS.USER_TEAM_NOT_FOUND);
+
+  const { homeTeam, awayTeam } = matchInfo;
+
+  const userTeam = [homeTeam, awayTeam].find((team) => team.id === userTeamId);
+  if (!userTeam) return createError(LINEUP_ERRORS.INVALID_TEAM);
+
+  const teamPlayers = await getTeamsPlayers([userTeamId]);
+  const teamPlayersIds = new Set(teamPlayers.map((player) => player.id));
+
+  const areInvalidPlayers = lineupPlayers.some(
+    (player) => !teamPlayersIds.has(player.id)
+  );
+  if (areInvalidPlayers) return createError(LINEUP_ERRORS.INVALID_PLAYERS);
+
+  return createSuccess("", {
+    lineupId: userTeam.lineup?.id ?? null,
+    userTeamId,
+  });
+}
