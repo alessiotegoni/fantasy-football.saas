65abb5f02a70a2d2c60e4494e2a3d706461aaf14
tega <alessiotegoni4@gmail.com>
2025-07-29 18:40:37 +0200

    add: basePermissions permission fun

--- src/features/(league)/(admin)/calculate-matchday/permissions/calculate-matchday.ts
+++ src/features/(league)/(admin)/calculate-matchday/permissions/calculate-matchday.ts
@@ -1,6 +1,26 @@
 import { db } from "@/drizzle/db";
 import { leagueMatchdayCalculations } from "@/drizzle/schema";
+import { getLeagueAdmin } from "@/features/(league)/leagues/queries/league";
+import { getUserId } from "@/features/users/utils/user";
+import { createError, createSuccess } from "@/lib/helpers";
+import { VALIDATION_ERROR } from "@/schema/helpers";
 import { and, count, eq } from "drizzle-orm";
+
+enum CALCULATE_ERRORS {
+  REQUIRE_ADMIN = "Per calcolare le giornate devi essere un admin della lega",
+}
+
+async function basePermissions(leagueId: string) {
+  const userId = await getUserId();
+  if (!userId) return createError(VALIDATION_ERROR);
+
+  if (!(await getLeagueAdmin(userId, leagueId))) {
+    return createError(CALCULATE_ERRORS.REQUIRE_ADMIN);
+  }
+
+  return createSuccess("", null);
+}
+
 
 export async function isAlreadyCalculated(
   leagueId: string,
