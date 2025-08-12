51560999106ffc38e37655f8b926b18bb8f4a18a
tega <alessiotegoni4@gmail.com>
2025-08-12 09:08:32 +0200

    fix: auction cache utils fn

--- src/features/(league)/auctions/db/cache/auction.ts
+++ src/features/(league)/auctions/db/cache/auction.ts
@@ -1,11 +1,18 @@
-import { getLeagueTag } from "@/cache/helpers";
+import { getIdTag, getLeagueTag } from "@/cache/helpers";
 import { revalidateTag } from "next/cache";
 
-export type AUCTION_TAG = "auctions"
+export type AUCTION_TAG = "auctions";
 
-export const getLeagueAuctionTag = (leagueId: string) =>
+export const getLeagueAuctionsTag = (leagueId: string) =>
   getLeagueTag("auctions", leagueId);
 
-export const revalidateLeagueAuctionTag = (leagueId: string) => {
-  revalidateTag(getLeagueAuctionTag(leagueId));
+export const getAuctionIdTag = (auctionId: string) =>
+  getIdTag("auctions", auctionId);
+
+export const revalidateLeagueAuctionsTag = (
+  leagueId: string,
+  auctionId: string
+) => {
+  revalidateTag(getLeagueAuctionsTag(leagueId));
+  revalidateTag(getAuctionIdTag(auctionId));
 };
