eb9af9942d9cd13b0e51c3a155a7f427d34072cd
tega <alessiotegoni4@gmail.com>
2025-08-12 17:11:30 +0200

    add: auctionSettings db functions

---
+++ src/features/(league)/auctions/db/auctionSettings.ts
@@ -0,0 +1,47 @@
+import { db } from "@/drizzle/db";
+import { auctionSettings } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
+import { createError } from "@/lib/helpers";
+import { revalidateAuctionSettingsCache } from "./cache/auction";
+
+enum DB_ERROR_MESSAGES {
+  CREATION_FAILED = "Errore nella creazione delle impostazioni",
+  UPDATE_FAILED = "Errore nell'aggiornamento delle impostazioni",
+}
+
+const settingsInfo = {
+  auctionId: auctionSettings.auctionId,
+};
+
+export async function insertAuctionSettings(
+  settings: typeof auctionSettings.$inferInsert,
+  tx: Omit<typeof db, "$client"> = db
+) {
+  const [result] = await tx
+    .insert(auctionSettings)
+    .values(settings)
+    .returning(settingsInfo);
+
+  if (!result?.auctionId) {
+    throw new Error(createError(DB_ERROR_MESSAGES.CREATION_FAILED).message);
+  }
+
+  revalidateAuctionSettingsCache(result.auctionId);
+}
+
+export async function updateAuctionSettings(
+  auctionId: string,
+  settings: Partial<typeof auctionSettings.$inferInsert>
+) {
+  const [result] = await db
+    .update(auctionSettings)
+    .set(settings)
+    .where(eq(auctionSettings.auctionId, auctionId))
+    .returning(settingsInfo);
+
+  if (!result?.auctionId) {
+    throw new Error(createError(DB_ERROR_MESSAGES.UPDATE_FAILED).message);
+  }
+
+  revalidateAuctionSettingsCache(result.auctionId);
+}
