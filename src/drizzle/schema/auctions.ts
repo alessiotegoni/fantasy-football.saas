1fadfcd2f9f24a75f07b201a3487a71473f29d7c
tega <alessiotegoni4@gmail.com>
2025-08-12 10:11:49 +0200

    fix: auctions db schema

--- src/drizzle/schema/auctions.ts
+++ src/drizzle/schema/auctions.ts
@@ -1,8 +1,16 @@
-import { pgTable, uuid, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  uuid,
+  text,
+  timestamp,
+  pgEnum,
+  smallint,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { leagues } from "./leagues";
 import { leagueMemberTeams } from "./leagueMemberTeams";
 import { auctionNominations } from "./auctionNominations";
+import { splits } from "./splits";
 
 export const auctionTypes = ["waiting", "active", "paused", "ended"] as const;
 export type AuctionType = (typeof auctionTypes)[number];
@@ -14,6 +22,9 @@ export const auctions = pgTable("auctions", {
   leagueId: uuid("league_id")
     .notNull()
     .references(() => leagues.id, { onDelete: "cascade" }),
+  splitId: smallint("split_id")
+    .notNull()
+    .references(() => splits.id, { onDelete: "restrict" }),
   createdBy: uuid("created_by")
     .notNull()
     .references(() => leagueMemberTeams.id, { onDelete: "set null" }),
@@ -30,6 +41,10 @@ export const auctionsRelations = relations(auctions, ({ one, many }) => ({
   league: one(leagues, {
     fields: [auctions.leagueId],
     references: [leagues.id],
+  }),
+  split: one(splits, {
+    fields: [auctions.splitId],
+    references: [splits.id],
   }),
   creator: one(leagueMemberTeams, {
     fields: [auctions.createdBy],
---
+++ src/features/(league)/auctions/queries/auction.ts
@@ -0,0 +1,6 @@
+export async function getAuctionsSettings(leagueId: string) {
+    "use cache"
+
+    
+
+}
