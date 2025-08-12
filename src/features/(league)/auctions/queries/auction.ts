5d0ecc01a053dbbfbf7946ecbb38e6c9b6119590
tega <alessiotegoni4@gmail.com>
2025-08-12 14:02:21 +0200

    add: getAuctionSetting query fn

--- src/features/(league)/auctions/queries/auction.ts
+++ src/features/(league)/auctions/queries/auction.ts
@@ -1,9 +1,17 @@
-import { cacheTag } from "next/dist/server/use-cache/cache-tag"
-import { getAuctionSettingTag } from "../db/cache/auction"
+import { cacheTag } from "next/dist/server/use-cache/cache-tag";
+import { getAuctionSettingTag } from "../db/cache/auction";
+import { db } from "@/drizzle/db";
+import { auctionSettings } from "@/drizzle/schema";
+import { eq } from "drizzle-orm";
 
-export async function getAuctionsSettings(auctionId: string) {
-    "use cache"
-    cacheTag(getAuctionSettingTag(auctionId))
+export async function getAuctionSettings(auctionId: string) {
+  "use cache";
+  cacheTag(getAuctionSettingTag(auctionId));
 
+  const [result] = await db
+    .select()
+    .from(auctionSettings)
+    .where(eq(auctionSettings.auctionId, auctionId));
 
+  return result;
 }
