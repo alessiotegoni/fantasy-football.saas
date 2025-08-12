f6610fdc11e76cc54bfc4004ed937e24fcd04809
tega <alessiotegoni4@gmail.com>
2025-08-12 16:28:15 +0200

    fix: auction schema

--- src/features/(league)/auctions/schema/auction.ts
+++ src/features/(league)/auctions/schema/auction.ts
@@ -2,15 +2,22 @@ import { z } from "zod";
 import { initialCredits, playersPerRole } from "../../settings/schema/setting";
 
 const baseAuctionSchema = z.object({
-  name: z.string().min(4).max(20),
-  description: z.string().min(10).max(500).nullable(),
+  name: z
+    .string({ message: "Nome obbligatorio" })
+    .min(4, "Il nome deve avere almeno 4 caratteri")
+    .max(20, "Il nome non puo superare i 20 caratteri"),
+  description: z
+    .string()
+    .min(10, "La descrizione deve avere almeno 10 caratteri")
+    .max(500, "La descrizione non puo superare i 500 caratteri")
+    .nullable(),
   firstCallTime: z
-    .number()
+    .number({ message: "Numero invalido" })
     .int()
     .min(10, "Il tempo minimo della prima chiamata e' di 10 secondi")
     .max(60, "Il tempo massimo della prima chiamata e' di 1 minuto"),
   othersCallsTime: z
-    .number()
+    .number({ message: "Numero invalido" })
     .int()
     .min(5, "Il tempo minimo della altre chiamate e' di 5 secondi")
     .max(50, "Il tempo massimo delle altre chiamate e' di 50 secondi"),
