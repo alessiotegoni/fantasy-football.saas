9e63d0288f2ad4efa737a7e80cdc15bd810acaae
tega <alessiotegoni4@gmail.com>
2025-08-14 11:44:53 +0200

    fix: getLeagueAuction qury fn

--- src/features/(league)/auctions/queries/auction.ts
+++ src/features/(league)/auctions/queries/auction.ts
@@ -2,17 +2,23 @@ import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 import { getAuctionIdTag, getLeagueAuctionsTag } from "../db/cache/auction";
 import { db } from "@/drizzle/db";
 import { auctions } from "@/drizzle/schema";
-import { and, desc, eq } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 
 export async function getLeagueAuctions(leagueId: string, splitId: number) {
   "use cache";
   cacheTag(getLeagueAuctionsTag(leagueId));
 
-  const results = await db
-    .select()
-    .from(auctions)
-    .where(and(eq(auctions.leagueId, leagueId), eq(auctions.splitId, splitId)))
-    .orderBy(desc(auctions.startedAt));
+  const results = await db.query.auctions.findMany({
+    with: {
+      creator: {
+        columns: {
+          managerName: true,
+        },
+      },
+    },
+    where: (auction, { and, eq }) =>
+      and(eq(auction.leagueId, leagueId), eq(auction.splitId, splitId)),
+  });
 
   return results;
 }
@@ -25,3 +31,5 @@ export async function getAuction(id: string) {
 
   return result;
 }
+
+export type Auction = typeof auctions.$inferSelect;
