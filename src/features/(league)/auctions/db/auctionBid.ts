5bbac9fbc56c23948d7940efd8f77b524ac3fb88
tega <alessiotegoni4@gmail.com>
2025-08-20 09:26:25 +0200

    add: auctionBid db fn

---
+++ src/features/(league)/auctions/db/auctionBid.ts
@@ -0,0 +1,19 @@
+import { db } from "@/drizzle/db";
+import { auctionBids } from "@/drizzle/schema";
+import { createError } from "@/lib/helpers";
+
+enum DB_ERROR_MESSAGES {
+  CREATION_FAILED = "Errore nella creazione dell'offerta all'asta",
+}
+
+export async function insertBid(bid: typeof auctionBids.$inferInsert) {
+  const [result] = await db.insert(auctionBids).values(bid).returning({
+    bidId: auctionBids.id,
+  });
+
+  if (!result?.bidId) {
+    throw new Error(createError(DB_ERROR_MESSAGES.CREATION_FAILED).message);
+  }
+
+  return result.bidId;
+}
