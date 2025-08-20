c105f794ab30d5ccc64009072ada37c91d2fe26b
tega <alessiotegoni4@gmail.com>
2025-08-20 11:08:06 +0200

    fix: auctionBids db schema

--- src/drizzle/schema/auctionBids.ts
+++ src/drizzle/schema/auctionBids.ts
@@ -1,16 +1,16 @@
 import { pgTable, uuid, smallint, timestamp } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { auctionNominations } from "./auctionNominations";
-import { leagueMemberTeams } from "./leagueMemberTeams";
+import { auctionParticipants } from "./auctionParticipants";
 
 export const auctionBids = pgTable("auction_bids", {
   id: uuid("id").defaultRandom().primaryKey(),
   nominationId: uuid("nomination_id")
     .notNull()
     .references(() => auctionNominations.id, { onDelete: "cascade" }),
-  teamId: uuid("team_id")
+  participantId: uuid("participant_id")
     .notNull()
-    .references(() => leagueMemberTeams.id, { onDelete: "cascade" }),
+    .references(() => auctionParticipants.id, { onDelete: "cascade" }),
   amount: smallint("amount").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
     .defaultNow()
@@ -18,13 +18,13 @@ export const auctionBids = pgTable("auction_bids", {
 });
 
 export const auctionBidsRelations = relations(auctionBids, ({ one }) => ({
-  auctionNomination: one(auctionNominations, {
+  nomination: one(auctionNominations, {
     fields: [auctionBids.nominationId],
     references: [auctionNominations.id],
   }),
-  team: one(leagueMemberTeams, {
-    fields: [auctionBids.teamId],
-    references: [leagueMemberTeams.id],
+  participant: one(auctionParticipants, {
+    fields: [auctionBids.participantId],
+    references: [auctionParticipants.id],
   }),
 }));
 
