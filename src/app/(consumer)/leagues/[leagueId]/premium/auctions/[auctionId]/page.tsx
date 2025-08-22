8a02eb1e1716572fce242f834b86c9abe221f395
tega <alessiotegoni4@gmail.com>
2025-08-22 18:45:11 +0200

    fix: AuctionPage

--- src/app/(consumer)/leagues/[leagueId]/premium/auctions/[auctionId]/page.tsx
+++ src/app/(consumer)/leagues/[leagueId]/premium/auctions/[auctionId]/page.tsx
@@ -1,5 +1,11 @@
+import Container from "@/components/Container";
 import { getAuctionWithSettings } from "@/features/(league)/auctions/queries/auction";
+import { getAuctionParticipant } from "@/features/(league)/auctions/queries/auctionParticipant";
 import { getPlayersRoles } from "@/features/(league)/teamsPlayers/queries/teamsPlayer";
+import { getUserTeamId } from "@/features/users/queries/user";
+import { getUserId } from "@/features/users/utils/user";
+import { notFound, redirect } from "next/navigation";
+import { Suspense } from "react";
 
 type Props = {
   params: Promise<{
@@ -9,12 +15,43 @@ type Props = {
 };
 
 export default async function AuctionPage({ params }: Props) {
-  const { leagueId, auctionId } = await params;
+  const ids = await params;
 
   const [auction, playerRoles] = await Promise.all([
-    getAuctionWithSettings(auctionId),
+    getAuctionWithSettings(ids.auctionId),
     getPlayersRoles(),
   ]);
 
-  return <div>AuctionPage</div>;
+  if (!auction) notFound();
+
+  // TODO: prima mockup di ui con V0 passando lo screen di fantalab
+  // capire come funziona il realtime di supabase
+  // dopodiche creare query prima per i partecipanti in questa page
+  // mentre per i players dei partecipanti fare la query in un componente apparte wrappato
+  // con suspense
+  return (
+    <Container {...ids} headerLabel={auction.name} className="max-w-full">
+      <Suspense>
+        <SuspenseBoundary {...ids} />
+      </Suspense>
+    </Container>
+  );
+}
+
+async function SuspenseBoundary({
+  leagueId,
+  auctionId,
+}: {
+  leagueId: string;
+  auctionId: string;
+}) {
+  const userId = await getUserId();
+  if (!userId) return null;
+
+  const userTeamId = await getUserTeamId(userId, leagueId);
+
+  const userParticipant = await getAuctionParticipant(auctionId, userTeamId);
+  if (!userParticipant) redirect(`/leagues/${leagueId}/premium/auctions`);
+
+  return <></>;
 }
