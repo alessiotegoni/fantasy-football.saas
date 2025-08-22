216daad29636d59e78aceb21702a551d2900fffd
tega <alessiotegoni4@gmail.com>
2025-08-22 11:55:29 +0200

    fix: auctionNomination handler worker fn

--- src/features/(league)/auctions/tasks/workers/auctionNomination.ts
+++ src/features/(league)/auctions/tasks/workers/auctionNomination.ts
@@ -5,15 +5,12 @@ import { boss } from "@/lib/pg-boss";
 import { updateNomination } from "../../db/auctionNomination";
 import { db } from "@/drizzle/db";
 import { insertAcquisition } from "../../db/auctionAcquisition";
-import { Job, WorkHandler } from "pg-boss";
-
-export async function registerNominationExpiryWorker() {
-  const jobName = getNominationExpiryJobName();
-
-  await boss.work(jobName, handler);
-}
-
-registerNominationExpiryWorker();
+import { WorkHandler } from "pg-boss";
+import {
+  auctionAcquisitions,
+  auctionBids,
+  auctionNominations,
+} from "@/drizzle/schema";
 
 type NominationExpiryData = {
   nominationId: string;
@@ -28,28 +25,38 @@ const handler: WorkHandler<NominationExpiryData> = async ([job]) => {
   const highestBid = await getHighestBid(nominationId);
 
   await db.transaction(async (tx) => {
-    if (highestBid) {
-      await insertAcquisition(
-        {
-          auctionId: nomination.auctionId,
-          playerId: nomination.playerId,
-          participantId: highestBid.participantId,
-          price: highestBid.amount,
-        },
-        tx
-      );
-    } else {
-      await insertAcquisition(
-        {
-          auctionId: nomination.auctionId,
-          playerId: nomination.playerId,
-          participantId: nomination.nominatedBy,
-          price: nomination.initialPrice,
-        },
-        tx
-      );
-    }
-
+    const acquisitionData = getAcquisitionData(nomination, highestBid);
+    
+    await insertAcquisition(acquisitionData, tx);
     await updateNomination(nominationId, { status: "sold" }, tx);
   });
 };
+
+function getAcquisitionData(
+  nomination: typeof auctionNominations.$inferSelect,
+  highestBid: typeof auctionBids.$inferSelect | undefined
+): typeof auctionAcquisitions.$inferInsert {
+  if (highestBid) {
+    return {
+      auctionId: nomination.auctionId,
+      playerId: nomination.playerId,
+      participantId: highestBid.participantId,
+      price: highestBid.amount,
+    };
+  } else {
+    return {
+      auctionId: nomination.auctionId,
+      playerId: nomination.playerId,
+      participantId: nomination.nominatedBy,
+      price: nomination.initialPrice,
+    };
+  }
+}
+
+export async function registerNominationExpiryWorker() {
+  const jobName = getNominationExpiryJobName();
+
+  await boss.work(jobName, handler);
+}
+
+registerNominationExpiryWorker();
