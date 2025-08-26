aed99ce6e7ec35aaca148dabb9a997e0c5ad2a1b
tega <alessiotegoni4@gmail.com>
2025-08-26 10:17:52 +0200

    fix: AuctionHeader component

--- src/features/(league)/auctions/components/AuctionHeader.tsx
+++ src/features/(league)/auctions/components/AuctionHeader.tsx
@@ -6,6 +6,7 @@ import { AuctionWithSettings } from "../queries/auction";
 import { useSidebar } from "@/components/ui/sidebar";
 import AuctionStatus from "./AuctionStatus";
 import AuctionDropdownMenu from "./AuctionDropdownMenu";
+import { useEffect } from "react";
 
 type Props = {
   auction: NonNullable<AuctionWithSettings>;
@@ -13,24 +14,33 @@ type Props = {
 };
 
 export default function AuctionHeader({ auction, isAdmin }: Props) {
-  const { toggleSidebar } = useSidebar();
+  const { open, setOpen, toggleSidebar } = useSidebar();
+
+  useEffect(() => {
+    setOpen(false);
+
+    return () => setOpen(true);
+  }, []);
+
 
   return (
     <header className="flex items-center justify-between gap-4 p-4">
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-3">
         <Button variant="ghost" size="icon" onClick={toggleSidebar}>
           <Menu className="size-5" />
         </Button>
         <h2 className="text-2xl font-heading font-bold">{auction.name}</h2>
       </div>
-      <AuctionStatus auction={auction} canUpdate={isAdmin} />
-      {isAdmin && (
-        <AuctionDropdownMenu
-          auction={auction}
-          canUpdate={isAdmin}
-          leagueId={auction.leagueId}
-        />
-      )}
+      <div className="flex gap-3">
+        <AuctionStatus auction={auction} canUpdate={isAdmin} />
+        {isAdmin && (
+          <AuctionDropdownMenu
+            auction={auction}
+            canUpdate={isAdmin}
+            leagueId={auction.leagueId}
+          />
+        )}
+      </div>
     </header>
   );
 }
