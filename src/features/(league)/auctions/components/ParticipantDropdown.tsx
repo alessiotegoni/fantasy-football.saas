04254ff2efc7306ad6d50e0b1464d7aa8e7afedc
tega <alessiotegoni4@gmail.com>
2025-08-29 16:13:32 +0200

    fix: useAuctionParticipant hook

--- src/features/(league)/auctions/components/ParticipantDropdown.tsx
+++ src/features/(league)/auctions/components/ParticipantDropdown.tsx
@@ -80,20 +80,21 @@ function KickParticipantButton({ participant }: ParticipantActionProps) {
   const canKick = participant.teamId && participant.id !== userParticipant?.id;
 
   return (
-    <ActionButton
-      variant="destructive"
-      loadingText="Espello"
-      disabled={!canKick}
-      action={deleteParticipant.bind(null, {
-        auctionId: participant.auctionId,
-        teamId: participant.teamId!,
-      })}
-      className="text-sm px-2 py-1.5 rounded-lg w-full justify-start"
-      requireAreYouSure
-      areYouSureDescription="Il partecipante verra espulso solamente dall'asta e potra rientrare in un secondo momento"
-    >
-      <UserXmark />
-      Espelli partecipante
-    </ActionButton>
+    canKick && (
+      <ActionButton
+        variant="destructive"
+        loadingText="Espello"
+        action={deleteParticipant.bind(null, {
+          auctionId: participant.auctionId,
+          teamId: participant.teamId!,
+        })}
+        className="text-sm px-2 py-1.5 rounded-lg w-full justify-start"
+        requireAreYouSure
+        areYouSureDescription="Il partecipante verra espulso solamente dall'asta e potra rientrare in un secondo momento"
+      >
+        <UserXmark />
+        Espelli partecipante
+      </ActionButton>
+    )
   );
 }
