85f102f374c2de573b466c7ea3e1dd476abc589e
tega <alessiotegoni4@gmail.com>
2025-09-08 12:22:06 +0200

    fix: Dashboard layout

--- src/app/(consumer)/(private)/dashboard/layout.tsx
+++ src/app/(consumer)/(private)/dashboard/layout.tsx
@@ -1,6 +1,36 @@
-type Props = {}
-export default async function DashboardLayout({}: Props) {
+import {
+  isAdmin,
+  isContentCreator,
+  isRedaction,
+} from "@/features/users/utils/roles";
+import DashboardRolesProvider, {
+  Role,
+} from "@/contexts/DashboardRolesProvider";
+import { createClient } from "@/services/supabase/server/supabase";
+import { getUser } from "@/features/users/utils/user";
+import { redirect } from "next/navigation";
+
+type Props = {
+  children: React.ReactNode;
+};
+export default async function DashboardLayout({ children }: Props) {
+  const user = await getUser();
+  if (!user) redirect("/");
+
+  const supabase = await createClient();
+  const roles: Role[] = [];
+
+  const [admin, contentCreator, redaction] = await Promise.all([
+    isAdmin(supabase, user.id),
+    isContentCreator(supabase, user.id),
+    isRedaction(supabase, user.id),
+  ]);
+
+  if (admin) roles.push("admin");
+  if (contentCreator) roles.push("content-creator");
+  if (redaction) roles.push("redaction");
+
   return (
-    <div>DashboardLayout</div>
-  )
+    <DashboardRolesProvider roles={roles}>{children}</DashboardRolesProvider>
+  );
 }
