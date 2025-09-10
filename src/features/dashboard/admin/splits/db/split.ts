4bf5a289ab9b4f74720e2129cbf2a8cc1ac5a779
tega <alessiotegoni4@gmail.com>
2025-09-10 09:18:08 +0200

    add: split permission fn

---
+++ src/features/dashboard/admin/splits/db/split.ts
@@ -0,0 +1,23 @@
+import { db } from "@/drizzle/db";
+import { splits } from "@/drizzle/schema";
+import { createError } from "@/utils/helpers";
+import { revalidateSplitsCache } from "./cache/split";
+
+enum DB_ERRORS {
+  INSERT_ERROR = "Errore nella creazione dello split",
+  UPDATE_ERROR = "Errore nell'aggiormento dello split",
+  DELETE_ERROR = "Errore nell'eliminazione dello split",
+}
+
+export async function insertSplit(data: (typeof splits.$inferInsert)[]) {
+  const [res] = await db
+    .insert(splits)
+    .values(data)
+    .returning({ splitId: splits.id });
+
+  if (!res.splitId) {
+    throw new Error(createError(DB_ERRORS.INSERT_ERROR).message);
+  }
+
+  revalidateSplitsCache(res.splitId)
+}
---
+++ src/features/dashboard/admin/splits/permissions/split.ts
@@ -0,0 +1,24 @@
+import { getUserId } from "@/features/dashboard/user/utils/user";
+import { createError, createSuccess } from "@/utils/helpers";
+import { VALIDATION_ERROR } from "@/schema/helpers";
+import { isAdmin } from "@/features/dashboard/user/utils/roles";
+import { db } from "@/drizzle/db";
+import { createClient } from "@/services/supabase/server/supabase";
+
+enum SPLIT_ERRORS {
+  REQUIRE_ADMIN = "Devi essere un admin per gestire gli splits",
+}
+
+export async function canManageSplit() {
+  const userId = await getUserId();
+  if (!userId) return createError(VALIDATION_ERROR);
+
+  const supabase = await createClient();
+  const userIsAdmin = await isAdmin(supabase, userId);
+
+  if (!userIsAdmin) {
+    return createError(SPLIT_ERRORS.REQUIRE_ADMIN);
+  }
+
+  return createSuccess("", null);
+}
