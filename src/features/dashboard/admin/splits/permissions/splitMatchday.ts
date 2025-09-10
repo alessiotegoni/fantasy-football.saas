7279338889fdbdbc21d45bf0f385392e175d3809
tega <alessiotegoni4@gmail.com>
2025-09-10 10:43:28 +0200

    add: splitMatchday permission fn

---
+++ src/features/dashboard/admin/splits/permissions/splitMatchday.ts
@@ -0,0 +1,25 @@
+import { getUserId } from "@/features/dashboard/user/utils/user";
+import { createError, createSuccess } from "@/utils/helpers";
+import { VALIDATION_ERROR } from "@/schema/helpers";
+import { isAdmin, isSuperadmin } from "@/features/dashboard/user/utils/roles";
+import { createClient } from "@/services/supabase/server/supabase";
+
+enum SPLIT_MATCHDAY_ERRORS {
+  REQUIRE_ADMIN = "Devi essere un admin per gestire le giornate degli split",
+}
+
+export async function canManageSplitMatchday() {
+  const userId = await getUserId();
+  if (!userId) return createError(VALIDATION_ERROR);
+
+  if (isSuperadmin(userId)) return createSuccess("", null);
+
+  const supabase = await createClient();
+  const userIsAdmin = await isAdmin(supabase, userId);
+
+  if (!userIsAdmin) {
+    return createError(SPLIT_MATCHDAY_ERRORS.REQUIRE_ADMIN);
+  }
+
+  return createSuccess("", null);
+}
