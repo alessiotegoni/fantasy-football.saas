473dc71834fe50ee2a063b4a6ddc8e0ace5bb515
tega <alessiotegoni4@gmail.com>
2025-09-11 16:59:15 +0200

    add: BonusMalusTypeSelect component

---
+++ src/features/dashboard/admin/bonusMaluses/components/BonusMalusTypeSelect.tsx
@@ -0,0 +1,43 @@
+import { useFormContext } from "react-hook-form";
+import { BonusMalusType } from "../queries/bonusMalusType";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Combobox } from "@/components/ui/combobox";
+
+export default function BonusMalusTypeSelect({
+  types,
+}: {
+  types: BonusMalusType[];
+}) {
+  const form = useFormContext<{ bonusMalusTypeId: number }>();
+
+  const items = types.map((type) => ({
+    value: type.id.toString(),
+    label: type.name,
+  }));
+
+  return (
+    <FormField
+      control={form.control}
+      name="bonusMalusTypeId"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Bonus e malus</FormLabel>
+          <FormControl>
+            <Combobox
+              items={items}
+              value={field.value.toString()}
+              onSelect={field.onChange}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
