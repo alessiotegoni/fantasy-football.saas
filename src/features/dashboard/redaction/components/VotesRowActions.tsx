6bf3e9ea87d822c87fd5d5ee6e26c6bd78858c61
tega <alessiotegoni4@gmail.com>
2025-09-15 18:10:12 +0200

    fix: VoteRowActions component

--- src/features/dashboard/redaction/components/VotesRowActions.tsx
+++ src/features/dashboard/redaction/components/VotesRowActions.tsx
@@ -1,14 +1,27 @@
-import TableRowActions from "@/components/TableRowActions";
-import ActionButton from "@/components/ActionButton";
 import { TrashSolid } from "iconoir-react";
+import ActionButton from "@/components/ActionButton";
+import { SplitMatchday } from "../../admin/splits/queries/split";
+import { deleteVote } from "../votes/actions/vote";
+import TableRowActions from "@/components/TableRowActions";
+import { MatchdayVote } from "../queries/vote";
+import EditVoteDialog from "./EditVoteDialog";
 
-type Props = {};
+type Props = {
+  matchday: SplitMatchday;
+  vote: MatchdayVote;
+};
 
-export default function VotesRowActions({}: Props) {
+export default function VotesRowActions({ matchday, vote }: Props) {
+  if (matchday.status === "upcoming") return null;
+
   return (
     <TableRowActions>
+      <EditVoteDialog matchday={matchday} vote={vote} />
       <ActionButton
-        action={() => ""}
+        action={deleteVote.bind(null, {
+          voteId: vote.id,
+          matchdayId: matchday.id,
+        })}
         loadingText="Elimino..."
         requireAreYouSure
         className="px-2 py-1.5 text-sm rounded-lg justify-start"
