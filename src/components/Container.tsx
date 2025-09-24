145e2065cb30a15548efb963710162257a8bd1e9
tega <alessiotegoni4@gmail.com>
2025-09-24 10:29:26 +0200

    fix: Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,36 +1,46 @@
+"use client";
+
 import { cn } from "@/lib/utils";
+import { Href } from "@/utils/helpers";
 import { ArrowLeft } from "iconoir-react";
 import Link from "next/link";
-import { PropsWithChildren } from "react";
+import { useParams } from "next/navigation";
+import { PropsWithChildren, ReactNode } from "react";
 
 type Props = {
-  leagueId: string;
   headerLabel: string;
   showHeader?: boolean;
-  renderHeaderRight?: () => React.ReactNode;
+  headerRight?: ReactNode;
+  backLinkHref?: Href;
   className?: string;
 };
 
 export default function Container({
-  leagueId,
   headerLabel,
   className,
   showHeader = true,
-  renderHeaderRight,
+  headerRight,
+  backLinkHref,
   children,
 }: PropsWithChildren<Props>) {
+  const params = useParams();
+  const leagueId = params.leagueId as string | undefined;
+
   return (
     <div className={cn("max-w-[700px] mx-auto md:p-4", className)}>
       {showHeader && (
         <header className="flex justify-between items-center gap-2 mb-4 md:mb-8">
           <div className="flex items-center md:hidden">
-            <Link href={`/league/${leagueId}`} className="mr-3">
+            <Link
+              href={backLinkHref ?? (leagueId ? `/league/${leagueId}` : "/")}
+              className="mr-3"
+            >
               <ArrowLeft className="size-5" />
             </Link>
             <h2
               className={cn(
                 "text-2xl font-heading",
-                renderHeaderRight && "text-xl xs:text-2xl"
+                headerRight && "text-xl xs:text-2xl"
               )}
             >
               {headerLabel}
@@ -39,7 +49,7 @@ export default function Container({
           <h2 className="hidden md:block text-3xl font-heading">
             {headerLabel}
           </h2>
-          {renderHeaderRight?.()}
+          {headerRight}
         </header>
       )}
       {children}
