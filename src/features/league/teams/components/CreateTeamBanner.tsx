85da3fa87784a8ff237f3314b50ac0655bc3f31b
tega <alessiotegoni4@gmail.com>
2025-09-25 10:20:26 +0200

    add: CreateTeamBanner component

---
+++ src/features/league/teams/components/CreateTeamBanner.tsx
@@ -0,0 +1,30 @@
+import LinkButton from "@/components/LinkButton";
+import { NavArrowRight, Search } from "iconoir-react";
+
+export default function CreateTeamBanner({ leagueId }: { leagueId: string }) {
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-center p-6 md:p-4 bg-muted/30 rounded-2xl mb-4 md:mb-8">
+      <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4">
+        <div className="size-16 bg-muted rounded-full flex items-center justify-center">
+          <Search className="size-8 text-muted-foreground" />
+        </div>
+        <div className="text-center md:text-start">
+          <h3 className="text-lg md:text-xl font-heading">
+            Non hai ancora una squadra
+          </h3>
+          <p className="text-sm md:text-base text-muted-foreground">
+            Crea la tua squadra per partecipare alla Lega
+          </p>
+        </div>
+      </div>
+      <LinkButton
+        variant="gradient"
+        className="w-fit mt-6 md:mt-0 gap-4 p-2.5 md:py-3.5 md:px-4"
+        href={`/league/${leagueId}/teams/create`}
+      >
+        Crea la tua squadra
+        <NavArrowRight className="size-5" />
+      </LinkButton>
+    </div>
+  );
+}
--- src/features/league/teams/components/TeamsList.tsx
+++ src/features/league/teams/components/TeamsList.tsx
@@ -1,8 +1,6 @@
-import Link from "next/link";
 import { getLeagueTeams } from "../queries/leagueTeam";
-import { NavArrowRight, Search } from "iconoir-react";
-import { Button } from "@/components/ui/button";
 import LeagueTeamCard from "./LeagueTeamCard";
+import CreateTeamBanner from "./CreateTeamBanner";
 
 type Props = {
   teams: Awaited<ReturnType<typeof getLeagueTeams>>;
@@ -31,36 +29,6 @@ export default function TeamsList({ teams, leagueId, teamUserId }: Props) {
         ))}
       </div>
     </>
-  );
-}
-
-function CreateTeamBanner({ leagueId }: { leagueId: string }) {
-  return (
-    <div className="flex flex-col md:flex-row justify-between items-center p-6 md:p-4 bg-muted/30 rounded-2xl mb-4 md:mb-8">
-      <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4">
-        <div className="size-16 bg-muted rounded-full flex items-center justify-center">
-          <Search className="size-8 text-muted-foreground" />
-        </div>
-        <div className="text-center md:text-start">
-          <h3 className="text-lg md:text-xl font-heading">
-            Non hai ancora una squadra
-          </h3>
-          <p className="text-sm md:text-base text-muted-foreground">
-            Crea la tua squadra per partecipare alla Lega
-          </p>
-        </div>
-      </div>
-      <Button
-        variant="gradient"
-        asChild
-        className="w-fit mt-6 md:mt-0 gap-4 p-2.5 md:py-3.5 md:px-4"
-      >
-        <Link href={`/leagues/${leagueId}/teams/create`}>
-          Crea la tua squadra
-          <NavArrowRight className="size-5" />
-        </Link>
-      </Button>
-    </div>
   );
 }
 
