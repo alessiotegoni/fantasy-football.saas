885fc8ebe8dac484b7b54d6ae906cada4095a324
tega <alessiotegoni4@gmail.com>
2025-09-26 10:11:41 +0200

    fix: CreateTeamBanner component

--- src/features/league/teams/components/CreateTeamBanner.tsx
+++ src/features/league/teams/components/CreateTeamBanner.tsx
@@ -1,23 +1,15 @@
 import LinkButton from "@/components/LinkButton";
 import { NavArrowRight, Search } from "iconoir-react";
+import { Banner } from "@/components/banner";
 
 export default function CreateTeamBanner({ leagueId }: { leagueId: string }) {
   return (
-    
-    <div className="flex flex-col md:flex-row justify-between items-center p-6 md:p-4 bg-muted/30 rounded-3xl mb-4 md:mb-8">
-      <div className="flex flex-col md:flex-row items-center gap-2 md:gap-4">
-        <div className="size-16 bg-muted rounded-full flex items-center justify-center">
-          <Search className="size-8 text-muted-foreground" />
-        </div>
-        <div className="text-center md:text-start">
-          <h3 className="text-lg md:text-xl font-heading">
-            Non hai ancora una squadra
-          </h3>
-          <p className="text-sm md:text-base text-muted-foreground">
-            Crea la tua squadra per partecipare alla Lega
-          </p>
-        </div>
-      </div>
+    <Banner
+      icon={<Search className="size-8 text-muted-foreground" />}
+      title="Non hai ancora una squadra"
+      description="Crea la tua squadra per partecipare alla Lega"
+      className="mb-4 md:mb-8"
+    >
       <LinkButton
         variant="gradient"
         className="w-fit mt-6 md:mt-0 gap-4 py-3.5 px-4"
@@ -26,6 +18,6 @@ export default function CreateTeamBanner({ leagueId }: { leagueId: string }) {
         Crea la tua squadra
         <NavArrowRight className="size-5" />
       </LinkButton>
-    </div>
+    </Banner>
   );
 }
